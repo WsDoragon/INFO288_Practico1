201f8886c766cb49a2265003b99f7d615645184f
Jorge Gonzalez <54647620+WsDoragon@users.noreply.github.com>
2024-04-23 09:05:52 -0400

    Conexion a BSD

--- problema 1/slave1.py
+++ problema 1/slave1.py
@@ -15,9 +15,9 @@ base_de_datos =  [
     "Cuantica de los fluidos"
     ]
 
-# Load environment variables from .envslave file
+
+#Conexion a BSD
 load_dotenv('.envslave')
-#Conectar a la base de datos
 mydb = mysql.connector.connect(
     host=os.getenv('DB_HOST'),
     user=os.getenv('DB_USER'),
@@ -25,10 +25,40 @@ mydb = mysql.connector.connect(
     database=os.getenv('DB_NAME')
     )
 print(mydb)
-
+myCursor = mydb.cursor()
 
 
 @app.route('/searchDocs')
+def testDocs():
+    tipo_doc = request.args.get('tipo_doc')
+    print(os.getenv('DB_TABLE'))
+    myCursor.execute(f'SELECT titulo, tipo, nodo FROM {os.getenv("DB_TABLE")}')
+    docs = myCursor.fetchall()
+    resultados = []
+    for doc in docs:
+        if doc[1] == tipo_doc:
+            resultados.append({"titulo": doc[0], "tipo": doc[1], "nodo": doc[2]})
+    return jsonify(resultados)
+    
+@app.route('/searchTitulo')
+def testTitulo():
+    titulo = request.args.get('titulo')
+    titulo = unquote_plus(titulo)
+    palabras = titulo.split(' ')
+    myCursor.execute(f'SELECT titulo, tipo, nodo FROM {os.getenv("DB_TABLE")}')
+    docs = myCursor.fetchall()
+    resultados = []
+    #print(palabras)    
+    for doc in docs:
+        for palabra in palabras:
+            print(palabra)
+            if palabra.lower() in doc[0].lower():
+                print("ENTRE")
+                resultados.append({"titulo": doc[0], "tipo": doc[1], "nodo": doc[2]})
+                break
+    return jsonify(resultados)
+
+@app.route('/testDocs')
 def search():
     tipo_doc = request.args.get('tipo_doc')
     resultados = []
@@ -42,7 +72,7 @@ def search():
     return jsonify(resultados)
 
     
-@app.route('/searchTitulo')
+@app.route('/testTitulo')
 def searchTitle():
     titulo = request.args.get('titulo')
     palabras = titulo.split(' ')
@@ -61,5 +91,11 @@ def searchTitle():
             
     return jsonify(resultados)
 
+@app.route('/insertDoc', methods=['POST'])
+def insertDoc():
+    documento = request.json
+    base_de_datos.append(documento['titulo'])
+    return jsonify({"mensaje": "Documento insertado correctamente"})
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)  # Cambia el puerto para cada esclavo
+    app.run(debug=True, port=5001)  
