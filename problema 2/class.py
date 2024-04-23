c5165f5bb5d11a9ed119c941b2dbab80319a9ee5
leoxz98 <leoxz98@gmail.com>
2024-04-23 16:15:23 -0400

    avance clases ejercicio 2

---
+++ problema 2/class.py
@@ -0,0 +1,44 @@
+#clases server.py
+import random
+
+import random
+
+class Player:
+    def __init__(self, nickName, ip, port, ip_server, port_server):
+        self.nickName = nickName
+        self.ip = ip
+        self.port = port
+        self.ip_server = ip_server
+        self.port_server = port_server
+        self.turn = False
+        self.nDice = 0
+
+    def rollDice(self):
+        self.nDice = random.randint(1, 6)
+        self.sendPoints(self)
+        self.nDice = 0
+
+    def sendPoints(self):
+        print(f"mensaje a enviar:\n"
+              f"ip_server: {self.ip_server}\n"
+              f"ip_port: {self.port_server}\n"
+              f"puntos: {self.nDice}")
+
+class Team:
+    def __init__(self,id):
+        self.id = id
+        self.players = []
+        self.points = 0
+        self.turn = False
+
+    def play(self,turn): 
+        #lanzar los dados de todos los miembros
+        if(turn):
+            for x in self.players:
+                x.rollDice()
+            self.turn = False
+        pass
+    
+    
+
+
