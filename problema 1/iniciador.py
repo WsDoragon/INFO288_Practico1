c3ab305e72bea4173caf7fe34dfee16f0bfc0f42
Jorge Gonzalez <54647620+WsDoragon@users.noreply.github.com>
2024-04-23 10:45:06 -0400

    lectura de envs e iniciacion

--- problema 1/iniciador.py
+++ problema 1/iniciador.py
@@ -8,13 +8,18 @@ conda_env_name = "INFO288"
 
 # Comando para activar el entorno Conda
 activate_cmd = f"call conda activate {conda_env_name}"
+# Obtener todos los archivos .env en la carpeta slave_envs
+env_files = [f for f in os.listdir("slave_envs")]
 
-for i in range(1, 3):
+# Imprimir los nombres de los archivos .env encontrados
+for env_file in env_files:
+    print(env_file)
+for i in env_files:
     # Comando para ejecutar el archivo slave.py
-    execute_cmd = f"python slave{i}.py"
+    execute_cmd = f"python slave1.py ./slave_envs/{i}"
 
     # Obtener la ruta absoluta del archivo slave.py
-    file_path = os.path.abspath(f"slave{i}.py")
+    file_path = os.path.abspath(f"slave1.py")
 
     # Abrir una terminal de comandos, activar el entorno Conda y ejecutar los comandos
     os.system(f"start cmd /k \"{activate_cmd} && cd /D {os.path.dirname(file_path)} && {execute_cmd}\"")
