9c000b1b931f249b43c82cc7557c8fef363ce8c3
Jorge Gonzalez <berna2022@gmail.com>
2024-04-28 20:33:04 -0400

    Comentarios agregados

--- problema 1/master.py
+++ problema 1/master.py
@@ -26,6 +26,7 @@ mycursor = mydb.cursor()
 def index():
     return "Hola mundo!"
 
+# Ruta para registrar la direccion del nodo esclavo (url / ip y puerto)
 @app.route('/registrarNodo', methods=['POST'])
 def registrarNodo():
     print("Registrando nodo", request.args.get('url'), request.args.get('port'))
@@ -33,7 +34,7 @@ def registrarNodo():
     tipo_nodo = request.args.get('tipo_nodo')
     nodo_destino = request.args.get('url')+':'+request.args.get('port')
 
-        
+    # Verificacion de parametros
     if tipo_nodo and nodo_destino:
         mycursor.execute("UPDATE tipos SET nodoDestino = %s, updatedAt = NOW() WHERE nombre = %s", (nodo_destino, tipo_nodo.lower()))
         mydb.commit()
@@ -41,20 +42,23 @@ def registrarNodo():
     else:
         return jsonify({"Error": 'Falta tipo de documento o nodo destino'})
     
+# Ruta para insertar un documento nuevo
 @app.route('/insert', methods=['POST'])
 def insert():
     documento = request.json
     print(documento)
-    tipo = documento['tipo_doc']
+    tipo = documento['tipo_doc'] # Tipo de documento, uso para buscar el nodo destino
     mycursor.execute("SELECT nodoDestino FROM tipos WHERE nombre = %s", (tipo.lower(),))
     nodo_destino = mycursor.fetchone()
 
-    if nodo_destino:
-        response = requests.post('http://' + nodo_destino[0] + '/insertDoc', json=documento)
+    # Si encontramos el nodo destino, enviamos el documento
+    if nodo_destino: 
+        response = requests.post('http://' + nodo_destino[0] + '/insertDoc', json=documento) #Envio a nodo destino
         if response.status_code == 200:
             return jsonify({"status": "ok"})
         else:
             return jsonify({"Error": 'Error en la insercion'})
+    # Si no encontramos el nodo destino, retornamos un error
     else:
         return jsonify({"Error": 'Tipo de documento no encontrado'})
 
@@ -63,54 +67,39 @@ def test():
     try:
         tipo_doc = request.args.get('tipo_doc')
         titulo = request.args.get('titulo')
-        #print(titulo)
+        # Busqueda por tipo de documento
         if tipo_doc:
-            print("Query de tipo: ",tipo_doc)
+            #print("Query de tipo: ",tipo_doc)
             resultados = []
-            tiposDocs = tipo_doc.split(' ')
-            for tipo in tiposDocs:
-                mycursor.execute("SELECT nombre, nodoDestino FROM tipos WHERE nombre = %s", (tipo.lower(),))
+            tiposDocs = tipo_doc.split(' ') # Split de los tipos de documentos
+            for tipo in tiposDocs: # Busqueda de cada tipo de documento
+                mycursor.execute("SELECT nombre, nodoDestino FROM tipos WHERE nombre = %s", (tipo.lower(),)) # Busqueda del nodo destino
                 nodo = mycursor.fetchone()
                 print(nodo)
-                if nodo:
+                if nodo: # Si encontramos el nodo destino, enviamos la peticion
                     response = requests.get("http://" + nodo[1] + '/searchDocs', params={'tipo_doc': tipo})
                     if response.status_code == 200 and response.json() != []:
                         resultados.extend(response.json())
                     
-                    else:
+                    else: # Si el nodo esta vacio, agregamos un aviso a la lista a entregar
                         resultados.append([{"Hint": 'Base de datos se encuentra vacia', "tipo": tipo, "status": "200 - OK"}])
-                else:
+                else: # Si no encontramos el nodo destino, agregamos un error a la lista a entregar
                     resultados.append([{"Error": 'Tipo de documento no encontrado', "tipo": tipo, "status": "404 - Not Found"}])
             return jsonify(resultados)
         
-    
-            """
-            mycursor.execute("SELECT nombre, nodoDestino FROM tipos WHERE nombre = %s", (tipo.lower(),))
-                    esclavoTipo = mycursor.fetchone()
-                    print(esclavoTipo)
-                    if esclavoTipo:
-                        esclavo = esclavoTipo[1]
-                        response = requests.get(esclavo + '/searchDocs', params={'tipo_doc': tipo.lower()})
-                        if response.status_code == 200:
-                            return jsonify(response.json())
-                        else:
-                            return jsonify({"Error": 'Error en la consulta'})
-                    else:
-                        return jsonify({"Error": 'Tipo de documento no encontrado'})
-            """      
-
-            
+        # Busqueda por titulo   
         elif titulo:
-            mycursor.execute("SELECT nombre, nodoDestino FROM tipos")
+            mycursor.execute("SELECT nombre, nodoDestino FROM tipos") #Obtenemos todos los nodos esclavos
             esclavos = mycursor.fetchall()
             resultados = []
+            # recorremos todos los esclavos
             for esclavo in esclavos:
-                print(esclavo)
-                response = requests.get("http://" + esclavo[1] + '/searchTitulo', params={'titulo': titulo})
+                print("Consultando nodo: ", esclavo[0])
+                response = requests.get("http://" + esclavo[1] + '/searchTitulo', params={'titulo': titulo}) #enviamos la peticion al nodo
                 if response.status_code == 200:
                     resultados.extend(response.json())
             return jsonify(resultados)
-        else:
+        else: # Si no recibimos tipo_doc o titulo para query, retornamos un error
             return jsonify({"Error": 'No se especifico operacion'})
         
     except Exception as e:
--- problema 1/slave.py
+++ problema 1/slave.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 args = sys.argv
 if len(args) < 2:
-    print("Porfavor ingrese posicion .env\nEjemplo: python slave1.py ./slave_envs/.envslave1")
+    print("Porfavor ingrese posicion .env\nEjemplo: python slave.py ./slave_envs/.envslave1")
     sys.exit(1)
 print("Arguments:", args)
 
@@ -25,42 +25,46 @@ mydb = mysql.connector.connect(
 print(mydb)
 myCursor = mydb.cursor()
 
+# Ruta para buscar por tipo de documento
 @app.route('/searchDocs')
 def testDocs():
     tipo_doc = request.args.get('tipo_doc')
     print(os.getenv('DB_TABLE'))
     myCursor.execute(f'SELECT titulo, tipo, nodo FROM {os.getenv("DB_TABLE")}')
-    docs = myCursor.fetchall()
+    docs = myCursor.fetchall() # Obtenemos todos los documentos del nodo
     resultados = []
-    for doc in docs:
+    for doc in docs: # Agregamos cada doc al resultado si es del tipo solicitado
         if doc[1] == tipo_doc:
-            resultados.append({"titulo": doc[0], "tipo": doc[1], "nodo": doc[2]})
+            resultados.append({"titulo": doc[0], "tipo": doc[1], "nodo": doc[2]}) 
     return jsonify(resultados)
-    
+
+# Ruta para buscar por palabras en titulo
 @app.route('/searchTitulo')
 def testTitulo():
     titulo = request.args.get('titulo')
     titulo = unquote_plus(titulo)
-    palabras = titulo.split(' ')
+    palabras = titulo.split(' ') # Separamos las palabras del titulo
     myCursor.execute(f'SELECT titulo, tipo, nodo, autor FROM {os.getenv("DB_TABLE")}')
-    docs = myCursor.fetchall()
+    docs = myCursor.fetchall() #Obtenemos todos los documentos del nodo
     resultados = []
     
-    for doc in docs:
+    for doc in docs: # Agregamos cada doc al resultado si alguna palabra del titulo coincide
         for palabra in palabras:
             if palabra.lower() in doc[0].lower():
                 resultados.append({"titulo": doc[0], "tipo": doc[1], "nodo": doc[2], "autor": doc[3]})
                 break
     return jsonify(resultados)
 
+# Ruta para insertar documento nuevo
 @app.route('/insertDoc', methods=['POST'])
 def insertDoc():
     documento = request.json
     print(documento)
+    # Obtenemos parametros del json
     titulo = documento['titulo']
     tipo = documento['tipo_doc']
     autor = documento['autor']
-    nodo = "slave"+os.getenv('TIPO_NODO').capitalize()
+    nodo = "slave"+os.getenv('TIPO_NODO').capitalize() # Nodo actual
     myCursor.execute(f'INSERT INTO {os.getenv("DB_TABLE")} (titulo, tipo, autor, nodo) VALUES (%s, %s, %s, %s)', (titulo, tipo, autor, nodo))
     mydb.commit()
     return jsonify({"status": "ok"})
