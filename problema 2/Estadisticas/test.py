a0c1e2f11ddbba77ff16d092d9710d85a56be128
Jorge Gonzalez <berna2022@gmail.com>
2024-06-21 02:30:26 -0400

    avances

--- problema 2/Estadisticas/test.py
+++ problema 2/Estadisticas/test.py
@@ -18,21 +18,31 @@ def get_log_entries_by_intervals(log_file, interval_minutes):
 
     # Agrupar las entradas de registro por intervalos de tiempo
     entries_by_interval = defaultdict(list)
+    lastGoodEntry = ""
     for entry in log_entries:
+        count = 0
         if "SEND_GAME_STATS" in entry and "Points" in entry:
             timestamp_str = entry.split('|')[0].strip()
             timestamp = datetime.datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%S.%f')
             interval = calculate_interval(timestamp, first_timestamp, interval_minutes)
             entries_by_interval[interval].append(entry)
+            lastGoodEntry = entry
+            count += 1
+        if count == 0:
+            timestamp_str = entry.split('|')[0].strip()
+            timestamp = datetime.datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%S.%f')
+            interval = calculate_interval(timestamp, first_timestamp, interval_minutes)
+            if len(entries_by_interval[interval]) == 0:
+                entries_by_interval[interval].append(lastGoodEntry)
 
     return entries_by_interval
 
 # Ejemplo de uso
-interval_minutes = 2  # Intervalo de tiempo en minutos
+interval_minutes = 0.5  # Intervalo de tiempo en minutos
 log_file = '../gameLog.txt'  # Reemplaza con la ruta a tu archivo de registro
 
 entries_by_interval = get_log_entries_by_intervals(log_file, interval_minutes)
-
+print("entries_by_interval: ", entries_by_interval)
 for interval, entries in entries_by_interval.items():
     print(f"Intervalo {interval}:")
     for entry in entries:
